d8477037a4859efebf79d0996074cdd99b399e0c
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-10 10:13:37 -0500

    Update sat1.cpp

--- sat1.cpp
+++ sat1.cpp
@@ -11,18 +11,29 @@
 // Syntax:
 //
 //	Clause<k> clauses[n] = ... ;
-//	Clause<t> solve<n, k, t>(clauses);
+//	Solution<t> solution = solve<n, k, t>(clauses);
 //
 // where
 //
 //	k is the number of literals per clause,
 //	n is the number of clauses,
 //	t is the total number of distinct literals.
+//
+// solution will contain a clause representing
+// an assignment of truth to the variables that
+// renders all of the clauses true simultaneously,
+// or will indicate that no solution exists.
 
 template <int k>
 struct Clause {
 	int ids[k];
 	bool invs[k];
+};
+
+template <int k>
+struct Solution {
+	bool solved;
+	Clause<k> sol;
 };
 
 template <int n, int k, int t>
