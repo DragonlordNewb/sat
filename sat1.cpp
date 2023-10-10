77ed459a3f4f2054fb23839b5969b36e5665b6d4
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-10 10:09:35 -0500

    Update sat1.cpp

--- sat1.cpp
+++ sat1.cpp
@@ -1,15 +1,30 @@
-using namespace std;
+// Variables MUST be compactly ordered.
+// This means that if variable n is used,
+// variable 0, 1, 2, ..., n-2, and n-1
+// must also be used.
 
+// This is just a general syntax thing
+// so that the program works. All CNF
+// statements can be equivalently written
+// this way.
+
+// Syntax:
+//
+//	Clause<k> clauses[n] = ... ;
+//	Clause<t> solve<n, k, t>(clauses);
+//
+// where
+//
+//	k is the number of literals per clause,
+//	n is the number of clauses,
+//	t is the total number of distinct literals.
+
+template <int k>
 struct Clause {
-	int x;
-	int y;
-	int z;
-	bool xinv;
-	bool yinv;
-	bool zinv;
+	int ids[k];
+	bool invs[k];
 };
 
-template <int n, int k>
-Clause<k> solve(Clause<k> clauses[n]) {
-	const int
+template <int n, int k, int t>
+Clause<t> solve(Clause<k> clauses[n]) {
 }
