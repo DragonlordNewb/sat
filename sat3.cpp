3c5aab7a7e1b4e820239f71c2035193685ba7262
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-11 13:12:11 -0500

    Update sat3.cpp

--- sat3.cpp
+++ sat3.cpp
@@ -1,9 +1,25 @@
 template <typename T, const n>
 bool satisfiable(T variables[n]) {
+
+	// Satisfiable if 2^t > n * 2^(t - k)
+	
+	int t = 0;
+	
 	for (int i = 0; i < n; i++) {
+		if (variables[i] == 0) {
+			continue;
+		}
+		
 		for (int j = i + 1; i < n; i++) {
-			if (variables[i] == variables[j]) {0
+			if (variables[i] == variables[j]) {
+				variables[j] == 0;
 			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (variables[i] != 0) {
+			t++;
 		}
 	}
 }
