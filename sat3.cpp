653aab4690db9a2e03067d4acd7b80cc24938dc3
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-11 12:49:25 -0500

    Update sat3.cpp

--- sat3.cpp
+++ sat3.cpp
@@ -1,4 +1,9 @@
-template <typename T, const int n, const int m>
-bool satisfiable(T variables[n][m]) {
-	//
+template <typename T, const n>
+bool satisfiable(T variables[n]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; i < n; i++) {
+			if (variables[i] == variables[j]) {0
+			}
+		}
+	}
 }
