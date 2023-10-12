ab32977e053bd29e8d4a4392da64be1fe780c418
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-12 08:31:55 -0500

    im sad now

--- sat3.cpp
+++ sat3.cpp
@@ -3,3 +3,16 @@
 //	A positive integer refers to a variable of that index;
 //	A -1 is an OR operator; and
 //	A -2 is an AND operator.
+//
+// Since the problem is in conjunctive normal form, it is 
+// expected that there are parentheses surrounding any
+// consecutive string of OR operands.
+
+struct Result {
+	char resultCode;
+	bool satisfiable;
+};
+
+template <const int size>
+Result satisfiable(int tape[size]) {
+}
